7accdb6c4e071b2fa0326a3a629e70cd2d3250a2
catj0073@edu.zealand.dk <ZeaZEYVH!2>
2022-06-05 14:43:21 +0200

    Basic er på plads, men vi skal høre Kim om design/styling af knapperne og placeringen. Herudoverskalvi have tilføjet et billede til hvert produkt, så tekster og priser kan rettes til.

--- Click&Collect.php
+++ Click&Collect.php
@@ -1,33 +1,154 @@
-<!-- Instruktion til webbrowser om at vi kører HTML5 -->
 <!DOCTYPE html>
-
-<!-- html starter og slutter hele dokumentet / lang=da: Fortæller siden er på dansk -->
 <html lang="da">
 
-<!-- I <head> har man opsætning - det ser brugeren ikke, men det fortæller noget om siden -->
 <head>
-    <!-- Sætter tegnsætning til utf-8 som bl.a. tillader danske bogstaver -->
+
     <meta charset="utf-8">
 
-    <!-- Titel som ses oppe i browserens tab mv. -->
-    <title>Sigende titel</title>
-
-    <!-- Metatags der fortæller at søgemaskiner er velkomne, hvem der udgiver siden og copyright information -->
+    <title> Click & Collect </title>
+    <meta name="description" content="...">
     <meta name="robots" content="All">
     <meta name="author" content="Udgiver">
     <meta name="copyright" content="Information om copyright">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Sikrer man kan benytte CSS ved at tilkoble en CSS fil -->
+    <meta property="og:title" content="Chocolate Club | Click & Collect">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="...">
+    <meta property="og:image" content="...">
+    <meta property="og:description" content="...">
+    <meta property="og:locale" content="da.DK">
+
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
     <link href="css/styles.css" rel="stylesheet" type="text/css">
 
-    <!-- Sikrer den vises korrekt på mobil, tablet mv. ved at tage ift. skærmstørrelse - bliver brugt til responsive websider -->
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style> body{background-color: rgba(248,242,239,0.89)} </style>
+    <style> a:link{color: black; text-decoration: none;} </style>
+
 </head>
 
-<!-- i <body> har man alt indhold på siden som brugeren kan se -->
+
+<link href="https://fonts.googleapis.com/css2?family=Gentium+Plus:ital@0;1&family=Pathway+Gothic+One&display=swap" rel="stylesheet">
+
+
 <body>
 
-<!-- Her skal sidens indhold ligge -->
+    <?php include "includes/header.php"; ?>
+
+
+    <div class="container text-center mt-5 mb-5 ps-5 pe-5">
+        <h1 class="overskrift"> Click & Collect </h1>
+        <br>
+        <p class="tekst"> Hos Chocolate Club finder du en lækker blanding af meget forskelligt chokolade. Vi laver chokolade til alle aldersgrupper, til alle begivenheder og arrangementer, med rigtig mange forskellige smage og lige efter din smagsløg. Dyk ned i de mange fristelser!  </p>
+    </div>
+
+
+    <div class="container">
+        <div class="row">
+            <div class="col-6 col-md-4 col-xl-3 mb-5">
+                <img src="image/Chokolade.jpg" alt="Lækker chokolade">
+                <h2 class="overskrift text-center"> Mørk chokoladefrø </h2>
+                <p class="tekst text-center"> med flydende saltkaramel </p>
+                <br>
+                <p class="pris text-center "> 35,00 kr. </p>
+                <button type="button" class="btn btn-sm btn-dark active tekst" href="..."> Læs mere </button>
+            </div>
+            <div class="col-6 col-md-4 col-xl-3 mb-5">
+                <img src="image/Chokolade.jpg" alt="Lækker chokolade">
+                <h2 class="overskrift text-center"> Mørk chokoladefrø </h2>
+                <p class="tekst text-center"> med flydende saltkaramel </p>
+                <br>
+                <p class="pris text-center "> 35,00 kr. </p>
+                <button type="button" class="btn btn-sm btn-dark active tekst" href="..."> Læs mere </button>
+            </div>
+            <div class="col-6 col-md-4 col-xl-3 mb-5">
+                <img src="image/Chokolade.jpg" alt="Lækker chokolade">
+                <h2 class="overskrift text-center"> Mørk chokoladefrø </h2>
+                <p class="tekst text-center"> med flydende saltkaramel </p>
+                <br>
+                <p class="pris text-center "> 35,00 kr. </p>
+                <button type="button" class="btn btn-sm btn-dark active tekst" href="..."> Læs mere </button>
+            </div>
+            <div class="col-6 col-md-4 col-xl-3 mb-5">
+                <img src="image/Chokolade.jpg" alt="Lækker chokolade">
+                <h2 class="overskrift text-center"> Mørk chokoladefrø </h2>
+                <p class="tekst text-center"> med flydende saltkaramel </p>
+                <br>
+                <p class="pris text-center "> 35,00 kr. </p>
+                <button type="button" class="btn btn-sm btn-dark active tekst" href="..."> Læs mere </button>
+            </div>
+            <div class="col-6 col-md-4 col-xl-3 mb-5">
+                <img src="image/Chokolade.jpg" alt="Lækker chokolade">
+                <h2 class="overskrift text-center"> Mørk chokoladefrø </h2>
+                <p class="tekst text-center"> med flydende saltkaramel </p>
+                <br>
+                <p class="pris text-center "> 35,00 kr. </p>
+                <button type="button" class="btn btn-sm btn-dark active tekst" href="..."> Læs mere </button>
+            </div>
+            <div class="col-6 col-md-4 col-xl-3 mb-5">
+                <img src="image/Chokolade.jpg" alt="Lækker chokolade">
+                <h2 class="overskrift text-center"> Mørk chokoladefrø </h2>
+                <p class="tekst text-center"> med flydende saltkaramel </p>
+                <br>
+                <p class="pris text-center "> 35,00 kr. </p>
+                <button type="button" class="btn btn-sm btn-dark active tekst" href="..."> Læs mere </button>
+            </div>
+            <div class="col-6 col-md-4 col-xl-3 mb-5">
+                <img src="image/Chokolade.jpg" alt="Lækker chokolade">
+                <h2 class="overskrift text-center"> Mørk chokoladefrø </h2>
+                <p class="tekst text-center"> med flydende saltkaramel </p>
+                <br>
+                <p class="pris text-center "> 35,00 kr. </p>
+                <button type="button" class="btn btn-sm btn-dark active tekst" href="..."> Læs mere </button>
+            </div>
+            <div class="col-6 col-md-4 col-xl-3 mb-5">
+                <img src="image/Chokolade.jpg" alt="Lækker chokolade">
+                <h2 class="overskrift text-center"> Mørk chokoladefrø </h2>
+                <p class="tekst text-center"> med flydende saltkaramel </p>
+                <br>
+                <p class="pris text-center "> 35,00 kr. </p>
+                <button type="button" class="btn btn-sm btn-dark active tekst" href="..."> Læs mere </button>
+            </div>
+            <div class="col-6 col-md-4 col-xl-3 mb-5">
+                <img src="image/Chokolade.jpg" alt="Lækker chokolade">
+                <h2 class="overskrift text-center"> Mørk chokoladefrø </h2>
+                <p class="tekst text-center"> med flydende saltkaramel </p>
+                <br>
+                <p class="pris text-center "> 35,00 kr. </p>
+                <button type="button" class="btn btn-sm btn-dark active tekst" href="..."> Læs mere </button>
+            </div>
+            <div class="col-6 col-md-4 col-xl-3 mb-5">
+                <img src="image/Chokolade.jpg" alt="Lækker chokolade">
+                <h2 class="overskrift text-center"> Mørk chokoladefrø </h2>
+                <p class="tekst text-center"> med flydende saltkaramel </p>
+                <br>
+                <p class="pris text-center "> 35,00 kr. </p>
+                <button type="button" class="btn btn-sm btn-dark active tekst" href="..."> Læs mere </button>
+            </div>
+            <div class="col-6 col-md-4 col-xl-3 mb-5">
+                <img src="image/Chokolade.jpg" alt="Lækker chokolade">
+                <h2 class="overskrift text-center"> Mørk chokoladefrø </h2>
+                <p class="tekst text-center"> med flydende saltkaramel </p>
+                <br>
+                <p class="pris text-center "> 35,00 kr. </p>
+                <button type="button" class="btn btn-sm btn-dark active tekst" href="..."> Læs mere </button>
+            </div>
+            <div class="col-6 col-md-4 col-xl-3 mb-5">
+                <img src="image/Chokolade.jpg" alt="Lækker chokolade">
+                <h2 class="overskrift text-center"> Mørk chokoladefrø </h2>
+                <p class="tekst text-center"> med flydende saltkaramel </p>
+                <br>
+                <p class="pris text-center "> 35,00 kr. </p>
+                <button type="button" class="btn btn-sm btn-dark active tekst" href="..."> Læs mere </button>
+            </div>
+        </div>
+    </div>
+
+
+    <?php include "includes/footer.php"; ?>
+
+
+    <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 
 </body>
 </html>
