dad502ee3a56aeb1d64646b89acd8938497c032a
catj0073@edu.zealand.dk <ZeaZEYVH!2>
2022-06-05 14:47:01 +0200

    Header/Navigation er kodet, men der skal vi virkelig have hjælp fra Kim, så det står pænt i computer-version og kommer til at stå som vi ønsker i mobil-version.

--- includes/header.php
+++ includes/header.php
@@ -1,33 +1,55 @@
-<!-- Instruktion til webbrowser om at vi kører HTML5 -->
-<!DOCTYPE html>
 
-<!-- html starter og slutter hele dokumentet / lang=da: Fortæller siden er på dansk -->
-<html lang="da">
+<header>
 
-<!-- I <head> har man opsætning - det ser brugeren ikke, men det fortæller noget om siden -->
-<head>
-    <!-- Sætter tegnsætning til utf-8 som bl.a. tillader danske bogstaver -->
-    <meta charset="utf-8">
+    <nav class="navbar navbar-expand-lg navbar-light bg-header_footer">
 
-    <!-- Titel som ses oppe i browserens tab mv. -->
-    <title>Sigende titel</title>
+        <div class="container-fluid position-relative">
 
-    <!-- Metatags der fortæller at søgemaskiner er velkomne, hvem der udgiver siden og copyright information -->
-    <meta name="robots" content="All">
-    <meta name="author" content="Udgiver">
-    <meta name="copyright" content="Information om copyright">
+            <div class="w-100 d-flex justify-content-end">
 
-    <!-- Sikrer man kan benytte CSS ved at tilkoble en CSS fil -->
-    <link href="css/styles.css" rel="stylesheet" type="text/css">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
 
-    <!-- Sikrer den vises korrekt på mobil, tablet mv. ved at tage ift. skærmstørrelse - bliver brugt til responsive websider -->
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-</head>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0 w-100 justify-content-end">
+                        <li class="nav-item dropdown overskrift">
+                            <a class="nav-link dropdown-toggle" href="chokolade.php" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false"> CHOKOLADE </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="SmåtMenGodt.php"> SMÅT MEN GODT </a></li>
+                                <li><a class="dropdown-item" href="../HeltUdenTwist.php"> HELT UDEN TWIST </a></li>
+                                <li><a class="dropdown-item" href="BetydningsfuldeMinder.php"> BETYDNINGSFULDE MINDER </a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item overskrift">
+                            <a class="nav-link" href="Click&Collect.php"> CLICK & COLLECT </a>
+                        </li>
+                        <li class="nav-item overskrift">
+                            <a class="nav-link" href="Booking.php"> BOOKING </a>
+                        </li>
+                        <li class="nav-item overskrift">
+                            <a class="nav-link" href="OmOs.php"> OM OS </a>
+                        </li>
+                        <li class="nav-item overskrift">
+                            <a class="nav-link" href="Kontakt.php"> KONTAKT </a>
+                        </li>
+                    </ul>
+                </div>
 
-<!-- i <body> har man alt indhold på siden som brugeren kan se -->
-<body>
+            </div>
 
-<!-- Her skal sidens indhold ligge -->
+            <div>
+                <div>
+                    <a class="navbar-brand" href="..."> <img src="image/Shoppingbag.png" height="30px" width="30px" alt="Shoppingsbag / Kurv"> </a>
+                </div>
+            </div>
 
-</body>
-</html>
+            <div class="logo position-absolute">
+                <a class="navbar-brand" href="index.php"> <img src="image/ChocolateClub_Logo_Original_Transparent.png" alt="Chocolate Club logo" height="150px" width="150px"> </a>
+            </div>
+
+        </div>
+
+    </nav>
+
+</header>
