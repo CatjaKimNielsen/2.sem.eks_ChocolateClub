646de3a9e35644e876a405c5e6a7da60f0969589
catj0073@edu.zealand.dk <ZeaZEYVH!2>
2022-06-06 12:06:06 +0200

    Småt men godt er kodet færdig, der skal dog lige styles på knapperne

--- SmåtMenGodt.php
+++ SmåtMenGodt.php
@@ -1,33 +1,97 @@
-<!-- Instruktion til webbrowser om at vi kører HTML5 -->
 <!DOCTYPE html>
-
-<!-- html starter og slutter hele dokumentet / lang=da: Fortæller siden er på dansk -->
 <html lang="da">
 
-<!-- I <head> har man opsætning - det ser brugeren ikke, men det fortæller noget om siden -->
 <head>
-    <!-- Sætter tegnsætning til utf-8 som bl.a. tillader danske bogstaver -->
+
     <meta charset="utf-8">
 
-    <!-- Titel som ses oppe i browserens tab mv. -->
-    <title>Sigende titel</title>
-
-    <!-- Metatags der fortæller at søgemaskiner er velkomne, hvem der udgiver siden og copyright information -->
+    <title> Småt men godt </title>
+    <meta name="description" content="...">
     <meta name="robots" content="All">
     <meta name="author" content="Udgiver">
     <meta name="copyright" content="Information om copyright">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Sikrer man kan benytte CSS ved at tilkoble en CSS fil -->
+    <meta property="og:title" content="Chocolate Club | Småt men godt">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="...">
+    <meta property="og:image" content="...">
+    <meta property="og:description" content="...">
+    <meta property="og:locale" content="da.DK">
+
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
     <link href="css/styles.css" rel="stylesheet" type="text/css">
 
-    <!-- Sikrer den vises korrekt på mobil, tablet mv. ved at tage ift. skærmstørrelse - bliver brugt til responsive websider -->
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style> body{background-color: rgba(248,242,239,0.89)} </style>
+    <style> a:link{color: black; text-decoration: none;} a:hover{text-decoration: underline} </style>
+
 </head>
 
-<!-- i <body> har man alt indhold på siden som brugeren kan se -->
+
+<link href="https://fonts.googleapis.com/css2?family=Gentium+Plus:ital@0;1&family=Pathway+Gothic+One&display=swap" rel="stylesheet">
+
+
 <body>
 
-<!-- Her skal sidens indhold ligge -->
+    <?php include "includes/header.php"; ?>
+
+
+    <div class="container mt-5">
+        <div class="row mb-5">
+            <div class="col-6 col-sm-4 col-xl-3 mt-3 mb-2">
+                <img src="image/Chokoladefrø.jpg" alt="Chokoladefrø">
+            </div>
+            <div class="col-6 col-sm-4 col-xl-3 mt-2 mb-2 pt-4 text-center">
+                <h1 class="overskrift"> Småt men godt </h1>
+                <br>
+                <p class="tekst"> Fabelagtige chokolader til børn og barnlige sjæle. Vi producere chokolader lige efter dine ønsker og lige efter din smagsløg! </p>
+                <br>
+                <button type="button" class="btn btn-dark active tekst" href="..."> Book tid </button>
+            </div>
+            <div class="col-6 col-sm-4 col-xl-3 mt-3 mb-2">
+                <img src="image/Chokoladefrø.jpg" alt="Chokoladefrø">
+            </div>
+            <div class="col-6 col-sm-4 col-xl-3 mt-3 mb-2">
+                <img src="image/Chokoladefrø.jpg" alt="Chokoladefrø">
+            </div>
+            <div class="col-6 col-sm-4 col-xl-3 mt-3 mb-2">
+                <img src="image/Chokoladefrø.jpg" alt="Chokoladefrø">
+            </div>
+            <div class="col-6 col-sm-4 col-xl-3 mt-3 mb-2">
+                <img src="image/Chokoladefrø.jpg" alt="Chokoladefrø">
+            </div>
+            <div class="col-6 col-sm-4 col-xl-3 mt-3 mb-2">
+                <img src="image/Chokoladefrø.jpg" alt="Chokoladefrø">
+            </div>
+            <div class="col-6 col-sm-4 col-xl-3 mt-3 mb-2">
+                <img src="image/Chokoladefrø.jpg" alt="Chokoladefrø">
+            </div>
+            <div class="col-6 col-sm-4 col-xl-3 mt-3 mb-2">
+                <img src="image/Chokoladefrø.jpg" alt="Chokoladefrø">
+            </div>
+            <div class="col-6 col-sm-4 col-xl-3 mt-3 mb-2">
+                <img src="image/Chokoladefrø.jpg" alt="Chokoladefrø">
+            </div>
+            <div class="col-6 col-sm-4 col-xl-3 mt-2 mb-2 pt-4 text-center">
+                <h2 class="overskrift"> Click & Collect </h2>
+                <br>
+                <p class="tekst"> Løber din mund også allerede i vand efter de mange lækre chokolader? Så chop dem allerede nu og få dem leveret direkte til din hoveddøren. </p>
+                <br>
+                <button type="button" class="btn btn-dark active tekst" href="..."> Bestil nu </button>
+            </div>
+            <div class="col-6 col-sm-4 col-xl-3 mt-3 mb-2">
+                <img src="image/Chokoladefrø.jpg" alt="Chokoladefrø">
+            </div>
+        </div>
+    </div>
+
+
+    <?php include "includes/footer.php"; ?>
+
+
+    <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 
 </body>
+
+
 </html>
