359d58364a95162cbe78c85900faa101275e7943
catj0073@edu.zealand.dk <ZeaZEYVH!2>
2022-06-06 21:17:33 +0200

    Siden Chokolade er oprette, men jeg synes ikke at opsættet er godt uanset hvad jeg prøver! Kommer tilbage til det.

--- chokolade.php
+++ chokolade.php
@@ -1,33 +1,148 @@
-<!-- Instruktion til webbrowser om at vi kører HTML5 -->
 <!DOCTYPE html>
-
-<!-- html starter og slutter hele dokumentet / lang=da: Fortæller siden er på dansk -->
 <html lang="da">
 
-<!-- I <head> har man opsætning - det ser brugeren ikke, men det fortæller noget om siden -->
 <head>
-    <!-- Sætter tegnsætning til utf-8 som bl.a. tillader danske bogstaver -->
+
     <meta charset="utf-8">
 
-    <!-- Titel som ses oppe i browserens tab mv. -->
-    <title>Sigende titel</title>
-
-    <!-- Metatags der fortæller at søgemaskiner er velkomne, hvem der udgiver siden og copyright information -->
+    <title> Chokolade </title>
+    <meta name="description" content="...">
     <meta name="robots" content="All">
     <meta name="author" content="Udgiver">
     <meta name="copyright" content="Information om copyright">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Sikrer man kan benytte CSS ved at tilkoble en CSS fil -->
+    <meta property="og:title" content="Chocolate Club | Chokolade">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="...">
+    <meta property="og:image" content="...">
+    <meta property="og:description" content="...">
+    <meta property="og:locale" content="da.DK">
+
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
     <link href="css/styles.css" rel="stylesheet" type="text/css">
 
-    <!-- Sikrer den vises korrekt på mobil, tablet mv. ved at tage ift. skærmstørrelse - bliver brugt til responsive websider -->
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style> body{background-color: rgba(248,242,239,0.89)} </style>
+    <style> a:link{color: black; text-decoration: none;} a:hover{text-decoration: underline} </style>
+
 </head>
 
-<!-- i <body> har man alt indhold på siden som brugeren kan se -->
+
+<link href="https://fonts.googleapis.com/css2?family=Gentium+Plus:ital@0;1&family=Pathway+Gothic+One&display=swap" rel="stylesheet">
+
+
 <body>
 
-<!-- Her skal sidens indhold ligge -->
+    <?php include "includes/header.php"; ?>
+
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-12 mt-2 mb-2 pt-4 text-center">
+                <h1 class="overskrift"> Chocolate Club Chokolade </h1>
+                <br>
+                <p class="tekst"> Hos Chocolate Club er vi 3 chocolateirs med hver vores speciale - vi laver nemlig chokolade til alle. Både til børn og barnlig sjæle, til de kræsne og til alle former for arrangementer og begivenheder. Og det bedste af det hele er at du har muligheden for, at sætte dit helt eget præg på dine chokolader. Book en tid, så vi kan få dine chokoladedrømme opfyldt. </p>
+            </div>
+        </div>
+    </div>
+
+    <div class="container mt-3">
+        <div class="row mb-5">
+            <div class="col-6 col-sm-6 col-xl-3 mt-3 mb-2">
+                <img src="image/LækreChokolader.jpg" alt="Lækre blandet chokolader">
+            </div>
+            <div class="col-6 col-sm-6 col-xl-3 mt-3 mb-2">
+                <img src="image/LækreChokolader.jpg" alt="Lækre blandet chokolader">
+            </div>
+            <div class="col-6 col-sm-6 col-xl-3 mt-3 mb-2">
+                <img src="image/LækreChokolader.jpg" alt="Lækre blandet chokolader">
+            </div>
+            <div class="col-6 col-sm-6 col-xl-3 mt-3 mb-2">
+                <img src="image/LækreChokolader.jpg" alt="Lækre blandet chokolader">
+            </div>
+            <div class="col-6 col-sm-6 col-xl-3 mt-3 mb-2">
+                <img src="image/LækreChokolader.jpg" alt="Lækre blandet chokolader">
+            </div>
+            <div class="col-12 col-sm-12 col-xl-6 mt-2 mb-2 pt-4 text-center">
+                <h2 class="overskrift"> Helt uden twist </h2>
+                <br>
+                <p class="tekst"> For os er det vigtigt at der er chokolade til alle - derfor laver vi også chokolade til de kræsne. Her er der altså ingen twists i smagene og du skal derfor ikke være bange for, at få noget i munden som du ikke bryder dig om. </p>
+                <br>
+                <button type="button" class="btn btn-dark active tekst" href="..."> Se mere </button>
+            </div>
+            <div class="col-6 col-sm-6 col-xl-3 mt-3 mb-2">
+                <img src="image/LækreChokolader.jpg" alt="Lækre blandet chokolader">
+            </div>
+            <div class="col-6 col-sm-6 col-xl-3 mt-3 mb-2">
+                <img src="image/LækreChokolader.jpg" alt="Lækre blandet chokolader">
+            </div>
+            <div class="col-6 col-sm-6 col-xl-3 mt-3 mb-2">
+                <img src="image/LækreChokolader.jpg" alt="Lækre blandet chokolader">
+            </div>
+            <div class="col-6 col-sm-6 col-xl-3 mt-3 mb-2">
+                <img src="image/LækreChokolader.jpg" alt="Lækre blandet chokolader">
+            </div>
+            <div class="col-6 col-sm-6 col-xl-3 mt-3 mb-2">
+                <img src="image/LækreChokolader.jpg" alt="Lækre blandet chokolader">
+            </div>
+            <div class="col-6 col-sm-6 col-xl-3 mt-3 mb-2">
+                <img src="image/LækreChokolader.jpg" alt="Lækre blandet chokolader">
+            </div>
+            <div class="col-12 col-sm-12 col-xl-6 mt-2 mb-2 pt-4 text-center">
+                <h2 class="overskrift"> Betydningsfulde minder </h2>
+                <br>
+                <p class="tekst"> Vi mener at chokolade kan være med til at skabe nogen fantastiske minder. Kender du det, når du spiser ét stykke chokolade og simpelthen bare bliver nødt til at tage et mere? Det gør vi. </p>
+                <br>
+                <button type="button" class="btn btn-dark active tekst" href="..."> Se mere </button>
+            </div>
+            <div class="col-6 col-sm-6 col-xl-3 mt-3 mb-2">
+                <img src="image/LækreChokolader.jpg" alt="Lækre blandet chokolader">
+            </div>
+            <div class="col-6 col-sm-6 col-xl-3 mt-3 mb-2">
+                <img src="image/LækreChokolader.jpg" alt="Lækre blandet chokolader">
+            </div>
+            <div class="col-6 col-sm-6 col-xl-3 mt-3 mb-2">
+                <img src="image/LækreChokolader.jpg" alt="Lækre blandet chokolader">
+            </div>
+            <div class="col-6 col-sm-6 col-xl-3 mt-3 mb-2">
+                <img src="image/LækreChokolader.jpg" alt="Lækre blandet chokolader">
+            </div>
+            <div class="col-6 col-sm-6 col-xl-3 mt-3 mb-2">
+                <img src="image/LækreChokolader.jpg" alt="Lækre blandet chokolader">
+            </div>
+            <div class="col-6 col-sm-6 col-xl-3 mt-3 mb-2">
+                <img src="image/LækreChokolader.jpg" alt="Lækre blandet chokolader">
+            </div>
+            <div class="col-12 col-sm-12 col-xl-6 mt-2 mb-2 pt-4 text-center">
+                <h2 class="overskrift"> Småt men godt </h2>
+                <br>
+                <p class="tekst"> De lækre søde sager er ikke kun for voksne - de er også for børn og barnlige sjæle. Så hvorfor ikke forkæle med lidt chokolade med yndlingssmagene? Eller måske en æske med 6 forskellige chokolader? </p>
+                <br>
+                <button type="button" class="btn btn-dark active tekst" href="..."> Se mere </button>
+            </div>
+            <div class="col-6 col-sm-6 col-xl-3 mt-3 mb-2">
+                <img src="image/LækreChokolader.jpg" alt="Lækre blandet chokolader">
+            </div>
+            <div class="col-6 col-sm-6 col-xl-3 mt-3 mb-2">
+                <img src="image/LækreChokolader.jpg" alt="Lækre blandet chokolader">
+            </div>
+            <div class="col-6 col-sm-6 col-xl-3 mt-3 mb-2">
+                <img src="image/LækreChokolader.jpg" alt="Lækre blandet chokolader">
+            </div>
+            <div class="col-6 col-sm-6 col-xl-3 mt-3 mb-2">
+                <img src="image/LækreChokolader.jpg" alt="Lækre blandet chokolader">
+            </div>
+            <div class="col-6 col-sm-6 col-xl-3 mt-3 mb-2">
+                <img src="image/LækreChokolader.jpg" alt="Lækre blandet chokolader">
+            </div>
+        </div>
+    </div>
+
+
+    <?php include "includes/footer.php"; ?>
+
+
+    <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 
 </body>
-</html>
+
+
