82a1018c42508005b61996cd013ef2b67e51ed6c
Susana <susa8837@edu.zealand.dk>
2022-06-07 13:41:49 +0200

    koder / styles til formularen

--- Kontakt.php
+++ Kontakt.php
@@ -1,33 +1,84 @@
-<!-- Instruktion til webbrowser om at vi kører HTML5 -->
-<!DOCTYPE html>
 
-<!-- html starter og slutter hele dokumentet / lang=da: Fortæller siden er på dansk -->
+<!DOCTYPE html>
 <html lang="da">
 
-<!-- I <head> har man opsætning - det ser brugeren ikke, men det fortæller noget om siden -->
 <head>
-    <!-- Sætter tegnsætning til utf-8 som bl.a. tillader danske bogstaver -->
+
     <meta charset="utf-8">
 
-    <!-- Titel som ses oppe i browserens tab mv. -->
-    <title>Sigende titel</title>
-
-    <!-- Metatags der fortæller at søgemaskiner er velkomne, hvem der udgiver siden og copyright information -->
+    <title> Chocolate Club </title>
+    <meta name="description" content="...">
     <meta name="robots" content="All">
     <meta name="author" content="Udgiver">
     <meta name="copyright" content="Information om copyright">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Sikrer man kan benytte CSS ved at tilkoble en CSS fil -->
+    <meta property="og:title" content="Chocolate Club">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="...">
+    <meta property="og:image" content="...">
+    <meta property="og:description" content="...">
+    <meta property="og:locale" content="da.DK">
+
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
     <link href="css/styles.css" rel="stylesheet" type="text/css">
 
-    <!-- Sikrer den vises korrekt på mobil, tablet mv. ved at tage ift. skærmstørrelse - bliver brugt til responsive websider -->
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style> body{background-color: rgba(248,242,239,0.89)} </style>
+    <style> a:link{color: black; text-decoration: none;} a:hover{text-decoration: underline} </style>
+
 </head>
 
-<!-- i <body> har man alt indhold på siden som brugeren kan se -->
+
+<link href="https://fonts.googleapis.com/css2?family=Gentium+Plus:ital@0;1&family=Pathway+Gothic+One&display=swap" rel="stylesheet">
+
+
 <body>
 
-<!-- Her skal sidens indhold ligge -->
+    <?php include "includes/header.php"; ?>
+
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-12 col-sm-6 col-xl-6">
+                <div class="text-center p-4">
+                    <h1 class="overskrift">KONTANTFORMULAR</h1>
+                </div>
+
+                <div method="get" action="index.html">
+                    <label for="Navn" class="name">Fulde navn:</label>
+                    <br>
+                    <input id="Navn" class="formularbox w-50" name="Navn" type="text" placeholder="" value="">
+                    <br><br>
+                    <label for="email" class="tlf">Telefon nr:</label>
+                    <br>
+                    <input id="Navn" class="formularbox w-50" name="tlf" type="text" placeholder="" value="">
+                    <br><br>
+                    <label for="Navn" class="email">E-mail:</label>
+                    <br>
+                    <input name="email" class="formularbox w-50" type="email" id="email" placeholder="" value="">
+                    <br><br>
+                    <label for="Navn" class="subject">Emne:</label>
+                    <br>
+                    <input id="Navn" class="formularbox w-50" name="Navn" type="text" placeholder="" value="">
+                    <br><br>
+                    <label for="besked" class="besked">Din besked</label>
+                    <br>
+                    <textarea id="besked" class="formularbox w-100" name="besked" placeholder=""></textarea>
+                    <br><br>
+                    <input type="submit" class="sendknap" value="Send besked">
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+    <?php include "includes/footer.php"; ?>
+
+
+    <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 
 </body>
+
 </html>
