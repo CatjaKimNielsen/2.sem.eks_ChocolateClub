37efc3d4087c28a2c3aa97b020d5c46460d59536
catj0073@edu.zealand.dk <ZeaZEYVH!2>
2022-06-14 19:14:26 +0200

    Click & Collect; Vores egne billeder er kommet på i komprimeret format, der er tilføjet alt-tekster og det meste SEO er på plads, men der mangler lidt endnu.

--- Click&Collect.php
+++ Click&Collect.php
@@ -6,9 +6,9 @@
     <meta charset="utf-8">
 
     <title> Click & Collect </title>
-    <meta name="description" content="...">
+    <meta name="description" content="Chocolate Club byder på en shop med lækre og fristende chokolade. Det kan bruges til selvforkælelse, begivenheder, til dessert eller lige hvad ønsker.">
     <meta name="robots" content="All">
-    <meta name="author" content="Udgiver">
+    <meta name="author" content="Catja Kim Nielsen & Susana Buch">
     <meta name="copyright" content="Information om copyright">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -16,7 +16,7 @@
     <meta property="og:type" content="website">
     <meta property="og:url" content="...">
     <meta property="og:image" content="...">
-    <meta property="og:description" content="...">
+    <meta property="og:description" content="Chocolate Club byder på en shop med lækre og fristende chokolade. Det kan bruges til selvforkælelse, begivenheder, til dessert eller lige hvad ønsker.">
     <meta property="og:locale" content="da.DK">
 
     <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
@@ -37,108 +37,110 @@
 
 
     <div class="container text-center mt-5 mb-5 ps-5 pe-5">
-        <h1 class="overskrift"> Click & Collect </h1>
+        <h1 class="overskrift"> Fristende og lækker chokolade til selvforkælelse, dessert og meget mere. </h1>
         <br>
-        <p class="tekst"> Hos Chocolate Club finder du en lækker blanding af meget forskelligt chokolade. Vi laver chokolade til alle aldersgrupper, til alle begivenheder og arrangementer, med rigtig mange forskellige smage og lige efter din smagsløg. Dyk ned i de mange fristelser!  </p>
+        <p class="tekst"> Hos Chocolate Club finder du en lækker blanding af meget fristende chokolader. Vi laver chokolade til alle
+            aldersgrupper, til alle begivenheder og arrangementer, med rigtig mange forskellige smage og lige efter din smagsløg.
+            Dyk ned i de mange fristelser!  </p>
     </div>
 
 
     <div class="container">
         <div class="row">
             <div class="col-6 col-md-4 col-xl-3 mb-5 justify-content-center">
-                <img src="image/Chokolade.jpg" alt="Lækker chokolade">
-                <h2 class="overskrift text-center"> Mørk chokoladefrø </h2>
-                <p class="tekst text-center"> med flydende saltkaramel </p>
+                <img src="image/Produkt_Æske_6stk.png" alt="Fyld selv æske med 6 chokolader">
+                <h2 class="overskrift text-center"> Fyld selv æske </h2>
+                <p class="tekst text-center"> Vælg selv 6 chokolader </p>
                 <br>
-                <p class="pris text-center "> 35,00 kr. </p>
+                <p class="pris text-center "> 125,00 kr. </p>
                 <a class="btn btn-sm bg-header_footer shadow w-100 tekst justify-content-center" href="..."> Læs mere </a>
             </div>
             <div class="col-6 col-md-4 col-xl-3 mb-5">
-                <img src="image/Chokolade.jpg" alt="Lækker chokolade">
-                <h2 class="overskrift text-center"> Mørk chokoladefrø </h2>
-                <p class="tekst text-center"> med flydende saltkaramel </p>
+                <img src="image/Produkt_Æske_5stk.png" alt="Fyld selv æske med 5 chokolader">
+                <h2 class="overskrift text-center"> Fyld selv æske </h2>
+                <p class="tekst text-center"> Vælg selv 5 chokolader </p>
                 <br>
-                <p class="pris text-center "> 35,00 kr. </p>
+                <p class="pris text-center "> 100 kr. </p>
                 <a class="btn btn-sm bg-header_footer shadow w-100 tekst justify-content-center" href="..."> Læs mere </a>
             </div>
             <div class="col-6 col-md-4 col-xl-3 mb-5">
-                <img src="image/Chokolade.jpg" alt="Lækker chokolade">
-                <h2 class="overskrift text-center"> Mørk chokoladefrø </h2>
-                <p class="tekst text-center"> med flydende saltkaramel </p>
+                <img src="image/Produkt_Marcipan.png" alt="Mørk chokolade med marcipan">
+                <h2 class="overskrift text-center"> Mørk chokolade </h2>
+                <p class="tekst text-center"> med fyld af ren marcipan </p>
                 <br>
-                <p class="pris text-center "> 35,00 kr. </p>
+                <p class="pris text-center "> 25,00 kr. </p>
                 <a class="btn btn-sm bg-header_footer shadow w-100 tekst justify-content-center" href="..."> Læs mere </a>
             </div>
             <div class="col-6 col-md-4 col-xl-3 mb-5">
-                <img src="image/Chokolade.jpg" alt="Lækker chokolade">
-                <h2 class="overskrift text-center"> Mørk chokoladefrø </h2>
-                <p class="tekst text-center"> med flydende saltkaramel </p>
+                <img src="image/Produkt_Chokolade_med_Kokos.png" alt="Lys chokolade med kokosfyld">
+                <h2 class="overskrift text-center"> Lys chokolade </h2>
+                <p class="tekst text-center"> med fyld af kokos </p>
                 <br>
-                <p class="pris text-center "> 35,00 kr. </p>
+                <p class="pris text-center "> 25,00 kr. </p>
                 <a class="btn btn-sm bg-header_footer shadow w-100 tekst justify-content-center" href="..."> Læs mere </a>
             </div>
             <div class="col-6 col-md-4 col-xl-3 mb-5">
-                <img src="image/Chokolade.jpg" alt="Lækker chokolade">
-                <h2 class="overskrift text-center"> Mørk chokoladefrø </h2>
-                <p class="tekst text-center"> med flydende saltkaramel </p>
+                <img src="image/Produkt_Mælkechokolade_med_mandel.png" alt="Mælkechokolade med en hel mandel i midten">
+                <h2 class="overskrift text-center"> Lys chokolade </h2>
+                <p class="tekst text-center"> med en hel mandel </p>
                 <br>
-                <p class="pris text-center "> 35,00 kr. </p>
+                <p class="pris text-center "> 25,00 kr. </p>
                 <a class="btn btn-sm bg-header_footer shadow w-100 tekst justify-content-center" href="..."> Læs mere </a>
             </div>
             <div class="col-6 col-md-4 col-xl-3 mb-5">
-                <img src="image/Chokolade.jpg" alt="Lækker chokolade">
-                <h2 class="overskrift text-center"> Mørk chokoladefrø </h2>
-                <p class="tekst text-center"> med flydende saltkaramel </p>
+                <img src="image/Produkt_MørkChokolade.png" alt="Ren mørk chokolade">
+                <h2 class="overskrift text-center"> Mørk chokolade </h2>
+                <p class="tekst text-center"> helt ren kvalitetschokolade </p>
                 <br>
-                <p class="pris text-center "> 35,00 kr. </p>
+                <p class="pris text-center "> 25,00 kr. </p>
                 <a class="btn btn-sm bg-header_footer shadow w-100 tekst justify-content-center" href="..."> Læs mere </a>
             </div>
             <div class="col-6 col-md-4 col-xl-3 mb-5">
-                <img src="image/Chokolade.jpg" alt="Lækker chokolade">
-                <h2 class="overskrift text-center"> Mørk chokoladefrø </h2>
-                <p class="tekst text-center"> med flydende saltkaramel </p>
+                <img src="image/Produkt_Nøddekugle.png" alt="Chokoladekugle med blandet nødder">
+                <h2 class="overskrift text-center"> Lys chokolade </h2>
+                <p class="tekst text-center"> med blandet hakkede nødder </p>
                 <br>
-                <p class="pris text-center "> 35,00 kr. </p>
+                <p class="pris text-center "> 25,00 kr. </p>
                 <a class="btn btn-sm bg-header_footer shadow w-100 tekst justify-content-center" href="..."> Læs mere </a>
             </div>
             <div class="col-6 col-md-4 col-xl-3 mb-5">
-                <img src="image/Chokolade.jpg" alt="Lækker chokolade">
-                <h2 class="overskrift text-center"> Mørk chokoladefrø </h2>
-                <p class="tekst text-center"> med flydende saltkaramel </p>
+                <img src="image/Produkt_Marcipan_med_Appelsin.png" alt="Hvid chokolade med marcipan og appelsin">
+                <h2 class="overskrift text-center"> Hvid chokolade </h2>
+                <p class="tekst text-center"> med marcipan og appelsin </p>
                 <br>
-                <p class="pris text-center "> 35,00 kr. </p>
+                <p class="pris text-center "> 25,00 kr. </p>
                 <a class="btn btn-sm bg-header_footer shadow w-100 tekst justify-content-center" href="..."> Læs mere </a>
             </div>
             <div class="col-6 col-md-4 col-xl-3 mb-5">
-                <img src="image/Chokolade.jpg" alt="Lækker chokolade">
-                <h2 class="overskrift text-center"> Mørk chokoladefrø </h2>
-                <p class="tekst text-center"> med flydende saltkaramel </p>
+                <img src="image/Produkt_Chokolade_med_karamel.png" alt="Mørk chokolade med flydende karamel">
+                <h2 class="overskrift text-center"> Mørk chokolade </h2>
+                <p class="tekst text-center"> med flydende karamel </p>
                 <br>
-                <p class="pris text-center "> 35,00 kr. </p>
+                <p class="pris text-center "> 25,00 kr. </p>
                 <a class="btn btn-sm bg-header_footer shadow w-100 tekst justify-content-center" href="..."> Læs mere </a>
             </div>
             <div class="col-6 col-md-4 col-xl-3 mb-5">
-                <img src="image/Chokolade.jpg" alt="Lækker chokolade">
-                <h2 class="overskrift text-center"> Mørk chokoladefrø </h2>
-                <p class="tekst text-center"> med flydende saltkaramel </p>
+                <img src="image/Produkt_Nougat_med_Hasselnød.png" alt="Lys chokolade med nougat og hel hasselnød">
+                <h2 class="overskrift text-center"> Lys chokolade </h2>
+                <p class="tekst text-center"> med nougat og hel hasselnød </p>
                 <br>
-                <p class="pris text-center "> 35,00 kr. </p>
+                <p class="pris text-center "> 25,00 kr. </p>
                 <a class="btn btn-sm bg-header_footer shadow w-100 tekst justify-content-center" href="..."> Læs mere </a>
             </div>
             <div class="col-6 col-md-4 col-xl-3 mb-5">
-                <img src="image/Chokolade.jpg" alt="Lækker chokolade">
-                <h2 class="overskrift text-center"> Mørk chokoladefrø </h2>
-                <p class="tekst text-center"> med flydende saltkaramel </p>
+                <img src="image/Produkt_Skildpadde_med_Mint.png" alt="Mørk chokoladeskildpadde med mint">
+                <h2 class="overskrift text-center"> Mørk chokoladeskildpadde </h2>
+                <p class="tekst text-center"> med flydende mintfyld </p>
                 <br>
-                <p class="pris text-center "> 35,00 kr. </p>
+                <p class="pris text-center "> 25,00 kr. </p>
                 <a class="btn btn-sm bg-header_footer shadow w-100 tekst justify-content-center" href="..."> Læs mere </a>
             </div>
             <div class="col-6 col-md-4 col-xl-3 mb-5">
-                <img src="image/Chokolade.jpg" alt="Lækker chokolade">
-                <h2 class="overskrift text-center"> Mørk chokoladefrø </h2>
-                <p class="tekst text-center"> med flydende saltkaramel </p>
+                <img src="image/Produkt_Mælkechokolade.png" alt="Lys chokoladefrø">
+                <h2 class="overskrift text-center"> Lys chokoladefrø </h2>
+                <p class="tekst text-center"> med et rent chokoladeindre </p>
                 <br>
-                <p class="pris text-center "> 35,00 kr. </p>
+                <p class="pris text-center "> 25,00 kr. </p>
                 <a class="btn btn-sm bg-header_footer shadow w-100 tekst justify-content-center" href="..."> Læs mere </a>
             </div>
         </div>
