6610ce153b9ce2698f2089e9831b6e8c4de8df51
catj0073@edu.zealand.dk <ZeaZEYVH!2>
2022-06-16 12:30:11 +0200

    Fejlside 404 er oprette og kodet.

---
+++ Fejl404.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="da">
+
+<head>
+
+    <meta charset="utf-8">
+
+    <title> Fejl 404 </title>
+    <meta name="description" content="Chocolate Club Fejl 404">
+    <meta name="robots" content="None">
+    <meta name="author" content="Catja Kim Nielsen & Susana Buch">
+    <meta name="copyright" content="Information om copyright">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+
+    <style> body{background-color: rgba(248,242,239,0.89)} </style>
+    <style> a:link{color: black; text-decoration: none;} a:hover{text-decoration: underline} </style>
+
+</head>
+
+
+<link href="https://fonts.googleapis.com/css2?family=Gentium+Plus:ital@0;1&family=Pathway+Gothic+One&display=swap" rel="stylesheet">
+
+
+<body>
+
+    <?php include "includes/header.php"; ?>
+
+
+    <section class="container text-center mt-5">
+        <div>
+            <h1 class="overskrift"> UPS - FEJL 404 </h1>
+            <br>
+            <p> Det ser ud som om der er nogen der har taget en bid af chokoladen. Nej, vi mener hjemmesiden. Gå til <a href="index.php">forsiden</a> og prøv igen.</p>
+            <br>
+            <img src="image/ChocolateClub_Fejl404_DerManglerEnBid.jpg" alt="Chokoladeskildpadder">
+        </div>
+    </section>
+
+
+    <?php include "includes/footer.php"; ?>
+
+</body>
+
+</html>
