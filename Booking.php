523589d72a9712b833b067a4c98fa50326a9bf28
catj0073@edu.zealand.dk <ZeaZEYVH!2>
2022-06-16 11:26:34 +0200

    Booking: De sidste detaljer er rettet til og SEO er færdigt.

--- Booking.php
+++ Booking.php
@@ -6,17 +6,17 @@
     <meta charset="utf-8">
 
     <title> Booking </title>
-    <meta name="description" content="...">
+    <meta name="description" content="Hos Chokolate Club laver vi personlig chokolade. Book en tid til møde i butikken og så finder vi sammen frem til hvordan dine ønsker skal formes i chokolade.">
     <meta name="robots" content="All">
-    <meta name="author" content="Udgiver">
+    <meta name="author" content="Catja Kim Nielsen & Susana Buch">
     <meta name="copyright" content="Information om copyright">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <meta property="og:title" content="Chocolate Club | Booking">
     <meta property="og:type" content="website">
-    <meta property="og:url" content="...">
-    <meta property="og:image" content="...">
-    <meta property="og:description" content="...">
+    <meta property="og:url" content="https://www.catjakimnielsen.dk/Eksamner/2.semester/Booking.php">
+    <meta property="og:image" content="https://www.catjakimnielsen.dk/Eksamner/2.semester/image/ChocolateClub_Logo_Original_Transparent.png">
+    <meta property="og:description" content="Hos Chokolate Club laver vi personlig chokolade. Book en tid til møde i butikken og så finder vi sammen frem til hvordan dine ønsker skal formes i chokolade.">
     <meta property="og:locale" content="da.DK">
 
     <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
@@ -36,18 +36,18 @@
     <?php include "includes/header.php"; ?>
 
 
-    <div class="container text-center mt-5 mb-5 ps-5 pe-5">
+    <section class="container text-center mt-5 mb-5 ps-5 pe-5">
         <h1 class="overskrift"> BOOKING </h1>
         <br>
-        <p class="tekst"> Hos Chocolate Club producere vi chokolader lige efter dine ønsker. Bestil tid og så finder vi frem til, hvordan vi skal udføre dine ønsker i den lækreste chokolade. </p>
-    </div>
+        <p class="tekst"> Hos Chocolate Club producere vi chokolader lige efter dine ønsker. Book tid og så finder vi frem til, hvordan vi skal udføre dine ønsker i den lækreste chokolade. </p>
+    </section>
 
 
-    <div class="container mt-2">
+    <section class="container mt-2">
         <div class="calendar row mx-auto">
             <div class="col-12 col-sm-6 col-xl-6">
                 <div class="text-center p-4">
-                    <h2 class="overskrift">VÆLG DATO</h2>
+                    <h2 class="overskrift">Vælg dato</h2>
                 </div>
                 <div class="calendar__opts tekst">
                     <select name="calendar__month" id="calendar__month">
@@ -116,39 +116,51 @@
                         <div class="calendar__date calendar__date--grey tekst"><span>2</span></div>
                         <div class="calendar__date calendar__date--grey tekst"><span>3</span></div>
                     </div>
+                    <div class="p-3">
+                        <label for="tidspunkt" class="subject tekst">Vælg tidspunkt</label>
+                        <br>
+                        <select id="tidspunkt" class="w-100 tekst" name="tidspunkt" type="text">
+                            <option class="tekst" value="tidspunkt"> kl. 10.00 </option>
+                            <option class="tekst" value="tidspunkt"> kl. 10.30 </option>
+                            <option class="tekst" value="tidspunkt"> kl. 11.00 </option>
+                            <option class="tekst" value="tidspunkt"> kl. 11.30 </option>
+                            <option class="tekst" value="tidspunkt"> kl. 12.30 </option>
+                            <option class="tekst" value="tidspunkt"> kl. 13.00 </option>
+                            <option class="tekst" value="tidspunkt"> kl. 13.30 </option>
+                            <option class="tekst" value="tidspunkt"> kl. 14.00 </option>
+                            <option class="tekst" value="tidspunkt"> kl. 14.30 </option>
+                            <option class="tekst" value="tidspunkt"> kl. 15.00 </option>
+                        </select>
+                    </div>
                 </div>
             </div>
 
             <div class="col-12 col-sm-6 col-xl-6">
                 <div class="text-center p-4">
-                    <h2 class="overskrift">DINE INFORMATIONER</h2>
+                    <h2 class="overskrift">Dine informationer</h2>
                 </div>
-                <div>
+                <form>
                     <label for="Navn" class="name tekst">Fulde navn</label>
                     <br>
                     <input id="Navn" class="formularboks w-100 tekst" name="Navn" type="text" placeholder="" value="">
                     <br><br>
-                    <label for="email" class="tlf tekst">Telefon nr</label>
+                    <label for="Nummer" class="tlf tekst">Telefon nr</label>
                     <br>
-                    <input id="Navn" class="formularboks w-100 tekst" name="tlf" type="text" placeholder="" value="">
+                    <input id="Nummer" class="formularboks w-100 tekst" name="Nummer" type="text" placeholder="" value="">
                     <br><br>
-                    <label for="Navn" class="email tekst">E-mail</label>
+                    <label for="Email" class="email tekst">E-mail</label>
                     <br>
-                    <input name="email" class="formularboks w-100 tekst" type="email" id="email" placeholder="" value="">
+                    <input name="Email" class="formularboks w-100 tekst" type="email" id="Email" placeholder="" value="">
                     <br><br>
-                    <label for="Navn" class="subject tekst">Vælg tidspunkt</label>
-                    <br>
-                    <input id="Navn" class="formularboks w-100 tekst" name="Navn" type="text" placeholder="" value="">
-                    <br><br>
-                    <label for="besked" class="besked tekst">Din besked</label>
+                    <label for="besked" class="besked tekst">Dine kommentarer til mødet</label>
                     <br>
                     <textarea id="besked" class="formularboks w-100 tekst" name="besked" placeholder=""></textarea>
                     <br><br>
                     <input type="submit" class="btn btn-sm bg-header_footer shadow w-50 tekst" value="Bekræft booking">
-                </div>
+                </form>
             </div>
         </div>
-    </div>
+    </section>
 
 
     <?php include "includes/footer.php"; ?>
