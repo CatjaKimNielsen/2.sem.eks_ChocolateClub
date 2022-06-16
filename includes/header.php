036eee28694dfd7a62393c80804df37517149a3a
catj0073@edu.zealand.dk <ZeaZEYVH!2>
2022-06-16 12:40:59 +0200

    Header og Footer: er kodet så godt som de nu kan og det bliver ikke bedre for nu.

--- includes/header.php
+++ includes/header.php
@@ -3,21 +3,28 @@
 
     <nav class="navbar navbar-expand-lg navbar-light bg-header_footer">
 
-        <div class="container-fluid position-relative">
+        <div class="container-fluid position-relative justify-content-end">
 
-            <div class="w-100 d-flex justify-content-end">
+            <div class="d-flex justify-content-end text-end">
+                <div class="order-1 order-lg-2">
+                    <div>
+                        <a class="navbar-brand align-content-center" href="Fejl404.php"> <img class="mt-4" src="image/Shoppingbag.png" width="30px" height="34px" alt="Shoppingsbag / Kurv"> </a>
+                    </div>
+                </div>
 
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <button class="navbar-toggler order-2 order-lg-3 type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <div class="order-3 order-lg-1 collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0 w-100 justify-content-end">
                         <li class="nav-item dropdown overskrift">
-                            <a class="nav-link dropdown-toggle" href="chokolade.php" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false"> CHOKOLADE </a>
+                            <a class="nav-link dropdown-toggle text-end" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false"> CHOKOLADE </a>
                             <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="chokolade.php"> VORES CHOKOLADER </a></li>
+                                <div class="dropdown-divider"></div>
                                 <li><a class="dropdown-item" href="SmåtMenGodt.php"> SMÅT MEN GODT </a></li>
-                                <li><a class="dropdown-item" href="../HeltUdenTwist.php"> HELT UDEN TWIST </a></li>
+                                <li><a class="dropdown-item" href="HeltUdenTwist.php"> HELT UDEN TWIST </a></li>
                                 <li><a class="dropdown-item" href="BetydningsfuldeMinder.php"> BETYDNINGSFULDE MINDER </a></li>
                             </ul>
                         </li>
@@ -38,14 +45,8 @@
 
             </div>
 
-            <div>
-                <div>
-                    <a class="navbar-brand" href="..."> <img src="image/Shoppingbag.png" height="30px" width="30px" alt="Shoppingsbag / Kurv"> </a>
-                </div>
-            </div>
-
             <div class="logo position-absolute">
-                <a class="navbar-brand" href="index.php"> <img src="image/ChocolateClub_Logo_Original_Transparent.png" alt="Chocolate Club logo" height="150px" width="150px"> </a>
+                <a class="navbar-brand" href="index.php"> <img src="image/ChocolateClub_Logo_Original_Transparent.png" alt="Chocolate Club logo" height=150px width="150px"> </a>
             </div>
 
         </div>
