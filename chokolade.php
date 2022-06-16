1904c46d66d3962b138b5fd79a3b5496e4ba4292
catj0073@edu.zealand.dk <ZeaZEYVH!2>
2022-06-16 09:56:06 +0200

    Vores chokolade: Vores egne billeder er sat ind og der er lavet SEO.

--- chokolade.php
+++ chokolade.php
@@ -5,18 +5,18 @@
 
     <meta charset="utf-8">
 
-    <title> Chokolade </title>
-    <meta name="description" content="...">
+    <title> Vores chokolade </title>
+    <meta name="description" content="Vores chokolade er ALTID produceret af den bedste kvalitet. Vi producere konfekt, fyldte chokolader og ren chokolade lige efter din smagsløg.">
     <meta name="robots" content="All">
-    <meta name="author" content="Udgiver">
+    <meta name="author" content="Catja Kim Nielsen & Susana Buch">
     <meta name="copyright" content="Information om copyright">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <meta property="og:title" content="Chocolate Club | Chokolade">
+    <meta property="og:title" content="Chocolate Club | Vores chokolade">
     <meta property="og:type" content="website">
-    <meta property="og:url" content="...">
-    <meta property="og:image" content="...">
-    <meta property="og:description" content="...">
+    <meta property="og:url" content="https://www.catjakimnielsen.dk/Eksamner/2.semester/chokolade.php">
+    <meta property="og:image" content="https://www.catjakimnielsen.dk/Eksamner/2.semester/image/4chokolader.png">
+    <meta property="og:description" content="Vores chokolade er ALTID produceret af den bedste kvalitet. Vi producere konfekt, fyldte chokolader og ren chokolade lige efter din smagsløg.">
     <meta property="og:locale" content="da.DK">
 
     <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
@@ -35,107 +35,113 @@
 
     <?php include "includes/header.php"; ?>
 
-    <div class="container mt-5">
+    <section class="container mt-5">
         <div class="row">
             <div class="col-12 mt-2 mb-2 pt-4 text-center">
-                <h1 class="overskrift"> Chocolate Club Chokolade </h1>
+                <h1 class="overskrift"> KONFEKT, FYLDTE CHOKOLADER OG REN CHOKOLADE. </h1>
                 <br>
-                <p class="tekst"> Hos Chocolate Club er vi 3 chocolateirs med hver vores speciale - vi laver nemlig chokolade til alle. Både til børn og barnlig sjæle, til de kræsne og til alle former for arrangementer og begivenheder. Og det bedste af det hele er at du har muligheden for, at sætte dit helt eget præg på dine chokolader. Book en tid, så vi kan få dine chokoladedrømme opfyldt. </p>
+                <p class="tekst"> Hos Chocolate Club er vi 3 chocolatiers med hver vores speciale - vi laver nemlig chokolade til alle. Både til børn og barnlig sjæle, til de kræsne og til alle former for arrangementer og begivenheder. Og det bedste af det hele er at du har muligheden for, at sætte dit helt eget præg på dine chokolader. Book en tid, så vi kan få dine chokoladedrømme opfyldt. </p>
             </div>
         </div>
-    </div>
+    </section>
 
-    <div class="container mt-3">
+    <section class="container mt-3">
         <div class="row mb-5">
-            <div class="col-6 col-sm-6 col-xl-3 mt-3 mb-2">
-                <img src="image/LækreChokolader.jpg" alt="Lækre blandet chokolader">
+            <div class="col-6 col-sm-4 col-xl-3 mt-3 mb-2 d-none d-sm-block">
+                <img src="image/Marcipan.png" alt="Mørk chokolade med marcipan">
             </div>
-            <div class="col-6 col-sm-6 col-xl-3 mt-3 mb-2">
-                <img src="image/LækreChokolader.jpg" alt="Lækre blandet chokolader">
+            <div class="col-6 col-sm-4 col-xl-3 mt-3 mb-2">
+                <img src="image/4chokolader.png" alt="4 blandet chokolader">
             </div>
-            <div class="col-6 col-sm-6 col-xl-3 mt-3 mb-2">
-                <img src="image/LækreChokolader.jpg" alt="Lækre blandet chokolader">
+            <div class="col-6 col-sm-4 col-xl-3 mt-3 mb-2">
+                <img src="image/Chokolade_med_Kokos.png" alt="Lys chokolade med kokos">
             </div>
-            <div class="col-6 col-sm-6 col-xl-3 mt-3 mb-2">
-                <img src="image/LækreChokolader.jpg" alt="Lækre blandet chokolader">
+            <div class="col-6 col-sm-4 col-xl-3 mt-3 mb-2">
+                <img src="image/Nougat_med_Hasselnød.png" alt="Lys chokolade med nougat og hel hasselnød">
             </div>
-            <div class="col-6 col-sm-6 col-xl-3 mt-3 mb-2">
-                <img src="image/LækreChokolader.jpg" alt="Lækre blandet chokolader">
+            <div class="col-6 col-sm-4 col-xl-3 mt-3 mb-2">
+                <img src="image/Chokolade_med_karamel.png" alt="Mørk chokolader med karamel">
             </div>
-            <div class="col-12 col-sm-12 col-xl-6 mt-2 mb-2 pt-4 text-center">
+            <div class="col-6 col-sm-4 col-xl-3 mt-3 mb-2 d-none d-sm-block d-xl-none">
+                <img src="image/2chokolader.png" alt="2 mørke chokolader med marcipan">
+            </div>
+            <div class="col-12 col-sm-12 col-xl-6 mt-2 mb-4 pt-4 text-center">
                 <h2 class="overskrift"> Helt uden twist </h2>
                 <br>
                 <p class="tekst"> For os er det vigtigt at der er chokolade til alle - derfor laver vi også chokolade til de kræsne. Her er der altså ingen twists i smagene og du skal derfor ikke være bange for, at få noget i munden som du ikke bryder dig om. </p>
                 <br>
-                <button type="button" class="btn btn-dark active tekst" href="..."> Se mere </button>
+                <a class="btn btn-sm bg-header_footer shadow w-50 tekst" href="HeltUdenTwist.php"> Se mere </a>
             </div>
-            <div class="col-6 col-sm-6 col-xl-3 mt-3 mb-2">
-                <img src="image/LækreChokolader.jpg" alt="Lækre blandet chokolader">
+            <div class="col-6 col-sm-4 col-xl-3 mt-3 mb-2">
+                <img src="image/2chokolader.png" alt="2 mørke chokolader med marcipan">
             </div>
-            <div class="col-6 col-sm-6 col-xl-3 mt-3 mb-2">
-                <img src="image/LækreChokolader.jpg" alt="Lækre blandet chokolader">
+            <div class="col-6 col-sm-4 col-xl-3 mt-3 mb-2">
+                <img src="image/Mælkechokolade.png" alt="Chokoladefrø af mælkechokolade">
             </div>
-            <div class="col-6 col-sm-6 col-xl-3 mt-3 mb-2">
-                <img src="image/LækreChokolader.jpg" alt="Lækre blandet chokolader">
+            <div class="col-6 col-sm-4 col-xl-3 mt-3 mb-2">
+                <img src="image/Nøddekugle.png" alt="Lys chokolade med blandet hakkede nødder">
             </div>
-            <div class="col-6 col-sm-6 col-xl-3 mt-3 mb-2">
-                <img src="image/LækreChokolader.jpg" alt="Lækre blandet chokolader">
+            <div class="col-6 col-sm-4 col-xl-3 mt-3 mb-2">
+                <img src="image/5chokolader.png" alt="5 blandet chokolader">
             </div>
-            <div class="col-6 col-sm-6 col-xl-3 mt-3 mb-2">
-                <img src="image/LækreChokolader.jpg" alt="Lækre blandet chokolader">
+            <div class="col-6 col-sm-4 col-xl-3 mt-3 mb-2">
+                <img src="image/MørkChokolade.png" alt="ren mørk chokolade">
             </div>
-            <div class="col-6 col-sm-6 col-xl-3 mt-3 mb-2">
-                <img src="image/LækreChokolader.jpg" alt="Lækre blandet chokolader">
+            <div class="col-6 col-sm-4 col-xl-3 mt-3 mb-2">
+                <img src="image/4chokolader.png" alt="4 blandet chokolader">
             </div>
-            <div class="col-12 col-sm-12 col-xl-6 mt-2 mb-2 pt-4 text-center">
+            <div class="col-12 col-sm-12 col-xl-6 mt-2 mb-4 pt-4 text-center">
                 <h2 class="overskrift"> Betydningsfulde minder </h2>
                 <br>
                 <p class="tekst"> Vi mener at chokolade kan være med til at skabe nogen fantastiske minder. Kender du det, når du spiser ét stykke chokolade og simpelthen bare bliver nødt til at tage et mere? Det gør vi. </p>
                 <br>
-                <button type="button" class="btn btn-dark active tekst" href="..."> Se mere </button>
+                <a class="btn btn-sm bg-header_footer shadow w-50 tekst" href="HeltUdenTwist.php"> Se mere </a>
             </div>
-            <div class="col-6 col-sm-6 col-xl-3 mt-3 mb-2">
-                <img src="image/LækreChokolader.jpg" alt="Lækre blandet chokolader">
+            <div class="col-6 col-sm-4 col-xl-3 mt-3 mb-2 d-none d-sm-block">
+                <img src="image/Marcipan.png" alt="Mørk chokolade med marcipan">
             </div>
-            <div class="col-6 col-sm-6 col-xl-3 mt-3 mb-2">
-                <img src="image/LækreChokolader.jpg" alt="Lækre blandet chokolader">
+            <div class="col-6 col-sm-4 col-xl-3 mt-3 mb-2">
+                <img src="image/Nougat_med_Hasselnød.png" alt="Lys chokolade med nougat og hel hasselnød">
             </div>
-            <div class="col-6 col-sm-6 col-xl-3 mt-3 mb-2">
-                <img src="image/LækreChokolader.jpg" alt="Lækre blandet chokolader">
+            <div class="col-6 col-sm-4 col-xl-3 mt-3 mb-2">
+                <img src="image/2chokolader.png" alt="2 mørke chokolader med marcipan">
             </div>
-            <div class="col-6 col-sm-6 col-xl-3 mt-3 mb-2">
-                <img src="image/LækreChokolader.jpg" alt="Lækre blandet chokolader">
+            <div class="col-6 col-sm-4 col-xl-3 mt-3 mb-2">
+                <img src="image/Chokolade_med_karamel.png" alt="Mørk chokolade med flydende karamel">
             </div>
-            <div class="col-6 col-sm-6 col-xl-3 mt-3 mb-2">
-                <img src="image/LækreChokolader.jpg" alt="Lækre blandet chokolader">
+            <div class="col-6 col-sm-4 col-xl-3 mt-3 mb-2">
+                <img src="image/Mælkechokolade_med_mandel.png" alt="Mælkechokolader med ne hel mandel">
             </div>
-            <div class="col-6 col-sm-6 col-xl-3 mt-3 mb-2">
-                <img src="image/LækreChokolader.jpg" alt="Lækre blandet chokolader">
+            <div class="col-6 col-sm-4 col-xl-3 mt-3 mb-2 d-none d-sm-block">
+                <img src="image/Skildpadde_med_Mint.png" alt="Mørk chokoladeskildpadde med mint">
             </div>
-            <div class="col-12 col-sm-12 col-xl-6 mt-2 mb-2 pt-4 text-center">
+            <div class="col-12 col-sm-12 col-xl-6 mt-2 mb-4 pt-4 text-center">
                 <h2 class="overskrift"> Småt men godt </h2>
                 <br>
                 <p class="tekst"> De lækre søde sager er ikke kun for voksne - de er også for børn og barnlige sjæle. Så hvorfor ikke forkæle med lidt chokolade med yndlingssmagene? Eller måske en æske med 6 forskellige chokolader? </p>
                 <br>
-                <button type="button" class="btn btn-dark active tekst" href="..."> Se mere </button>
+                <a class="btn btn-sm bg-header_footer shadow w-50 tekst" href="HeltUdenTwist.php"> Se mere </a>
             </div>
-            <div class="col-6 col-sm-6 col-xl-3 mt-3 mb-2">
-                <img src="image/LækreChokolader.jpg" alt="Lækre blandet chokolader">
+            <div class="col-6 col-sm-4 col-xl-3 mt-3 mb-2">
+                <img src="image/5chokolader.png" alt="5 blandet chokolader">
             </div>
-            <div class="col-6 col-sm-6 col-xl-3 mt-3 mb-2">
-                <img src="image/LækreChokolader.jpg" alt="Lækre blandet chokolader">
+            <div class="col-6 col-sm-4 col-xl-3 mt-3 mb-2">
+                <img src="image/2chokolader.png" alt="2 mørke chokolader med marcipan">
             </div>
-            <div class="col-6 col-sm-6 col-xl-3 mt-3 mb-2">
-                <img src="image/LækreChokolader.jpg" alt="Lækre blandet chokolader">
+            <div class="col-6 col-sm-4 col-xl-3 mt-3 mb-2">
+                <img src="image/Mælkechokolade_med_mandel.png" alt="Lys chokolade med hel mandel">
             </div>
-            <div class="col-6 col-sm-6 col-xl-3 mt-3 mb-2">
-                <img src="image/LækreChokolader.jpg" alt="Lækre blandet chokolader">
+            <div class="col-6 col-sm-4 col-xl-3 mt-3 mb-2">
+                <img src="image/Skildpadde_med_Mint.png" alt="Mørk chokoladeskildpadde med mint">
             </div>
-            <div class="col-6 col-sm-6 col-xl-3 mt-3 mb-2">
-                <img src="image/LækreChokolader.jpg" alt="Lækre blandet chokolader">
+            <div class="col-6 col-sm-4 col-xl-3 mt-3 mb-2 d-none d-sm-block">
+                <img src="image/Nougat_med_Hasselnød.png" alt="Lys chokolade med nougat og hel hasselnød">
+            </div>
+            <div class="col-6 col-sm-4 col-xl-3 mt-3 mb-2 d-none d-sm-block d-xl-none">
+                <img src="image/MørkChokolade.png" alt="Ren mørk chokolade">
             </div>
         </div>
-    </div>
+    </section>
 
 
     <?php include "includes/footer.php"; ?>
